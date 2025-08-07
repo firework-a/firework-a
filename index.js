6a6f39542f5167efc913644384b76535e6bba5f6
firework-a <2737459675@qq.com>
2025-08-07 14:16:17 +0800

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	new file:   .github/workflows/main.yml
    	new file:   index.js

---
+++ index.js
@@ -0,0 +1,86 @@
+const thisYear = new Date().getFullYear();
+const startTimeOfThisYear = new Date(`${thisYear}-01-01T00:00:00+00:00`).getTime();
+const endTimeOfThisYear = new Date(`${thisYear}-12-31T23:59:59+00:00`).getTime();
+const progressOfThisYear = (Date.now() - startTimeOfThisYear) / (endTimeOfThisYear - startTimeOfThisYear);
+const progressBarOfThisYear = generateProgressBar();
+
+function generateProgressBar() {
+    const progressBarCapacity = 30;
+    const passedProgressBarIndex = parseInt(progressOfThisYear * progressBarCapacity);
+    const progressBar =
+        '█'.repeat(passedProgressBarIndex) +
+        '▁'.repeat(progressBarCapacity - passedProgressBarIndex);
+    return `{ ${progressBar} }`;
+}
+
+const readme = `\
+## Hi there 👋
+
+I am interested in finding the helpful tool in github,and do something for the public repository.I really learn a lot here.
+
+⏳ Year progress ${progressBarOfThisYear} ${(progressOfThisYear * 100).toFixed(2)} %
+
+---
+
+⏰ Updated on ${new Date().toUTCString()}
+
+---
+
+- The code language I interested
+<p algin="center">
+<a href="https://www.python.org/">
+    <img alt="Python" height="20" src="https://cdn.simpleicons.org/python" />
+</a>
+<a href="https://web.developers.google.cn/javascript?hl=zh-cn">
+    <img alt="Javascript" height="20" src="https://cdn.simpleicons.org/javascript" />
+</a>
+<a href="https://golang.google.cn/">
+    <img alt="Golang" height="20" src="https://cdn.simpleicons.org/go" />
+</a>
+</p>
+
+- The framework I use
+<p algin="center">
+<a href="https://cn.vuejs.org/">
+    <img alt="Vuejs" height="20" src="https://cdn.simpleicons.org/vue.js" />
+</a>
+<a href="https://gin-gonic.com/">
+    <img alt="Nextjs" height="20" src="https://cdn.simpleicons.org/next.js" />
+</a>
+<a href="https://gin-gonic.com/">
+    <img alt="Gin" height="20" src="https://cdn.simpleicons.org/gin" />
+</a>
+<a href="https://fastapi.tiangolo.com/zh/">
+    <img alt="Fastapi" height="20" src="https://cdn.simpleicons.org/fastapi" />
+</a>
+</p>
+
+- Also I'm interested in
+<p algin="center">
+<a href="#">
+    <img alt="Wegame" height="20" src="https://cdn.simpleicons.org/wegame" />
+</a>
+<a href="#">
+    <img alt="Gameloft" height="20" src="https://cdn.simpleicons.org/gameloft" />
+</a>
+<a href="#">
+    <img alt="Epic Games" height="20" src="https://cdn.simpleicons.org/epicgames" />
+</a>
+<a href="#">
+    <img alt="Steam" height="20" src="https://cdn.simpleicons.org/steam" />
+</a>
+<a href="#">
+    <img alt="Riot Games" height="20" src="https://cdn.simpleicons.org/riotgames" />
+</a>
+    <a href="#">
+    <img alt="Rockstar Games" height="20" src="https://cdn.simpleicons.org/rockstargames" />
+</a>
+</p>
+
+### My Github Contributions
+
+![](https://fastly.jsdelivr.net/gh/firework-a/firework-a/assets/github-contribution-grid-snake.svg)
+\
+`;
+
+console.log(readme);
